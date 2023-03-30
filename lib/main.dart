0e8fb62066b2d517b176276b2063657757d13c1e
Mohamed-Shalabi <mshalaby10@gmail.com>
2023-03-30 15:39:02 +0200

    Added the example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'global_blocs.dart';
+
+final CounterStatefulCubit cubit = CounterStatefulCubit(
+  CounterRepository(
+    CounterDataSource(),
+  ),
+);
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,12 +17,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return StatefulBlocProvider(
+      app: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -29,12 +39,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    cubit.increment();
   }
 
   @override
@@ -43,25 +49,84 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: StatefulBlocListener(
+        listener: (context, state) {
+          if (state is CounterState) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.counter.toString())),
+            );
+          }
+        },
+        body: StatefulBlocConsumer<CounterState>(
+          initialState: CounterInitialState(),
+          builder: (context, state) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'You have pushed the button this many times:',
+                  ),
+                  Text(
+                    '${state.counter}',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
-      );
+      ),
+    );
   }
+}
+
+abstract class CounterState extends GlobalState {
+  final int counter;
+
+  CounterState(this.counter);
+}
+
+class CounterInitialState extends CounterState {
+  CounterInitialState() : super(0);
+}
+
+class CounterIncrementState extends CounterState {
+  CounterIncrementState(super.counter);
+}
+
+class CounterDecrementState extends CounterState {
+  CounterDecrementState(super.counter);
+}
+
+class CounterStatefulCubit extends StatefulCubit {
+  CounterStatefulCubit(this._repository);
+
+  final CounterRepository _repository;
+
+  void increment() {
+    final newValue = _repository.increment();
+    emit(CounterIncrementState(newValue));
+  }
+}
+
+class CounterRepository {
+  CounterRepository(this._dataSource);
+
+  final CounterDataSource _dataSource;
+
+  int get counter => _dataSource.counter;
+
+  int increment() => _dataSource.increment();
+}
+
+class CounterDataSource {
+  int counter = 0;
+
+  int increment() => ++counter;
 }
