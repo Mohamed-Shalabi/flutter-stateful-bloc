67c47fedb529fb5e5338a1667106961644f00e9a
Mohamed-Shalabi <mshalaby10@gmail.com>
2023-03-31 16:45:31 +0200

    added state provider

--- lib/global_blocs.dart
+++ lib/global_blocs.dart
@@ -8,6 +8,7 @@ part 'stateful_bloc_widgets/stateful_bloc_provider.dart';
 part 'stateful_bloc_widgets/stateful_bloc_consumer.dart';
 part 'stateful_bloc_widgets/stateful_bloc_listener.dart';
 part 'extendable_blocs/extendable_blocs.dart';
+part 'utils/state_observer.dart';
 
 final StreamController<ExtendableState> _statesStreamController =
     StreamController.broadcast();
@@ -18,11 +19,39 @@ class _GlobalCubit extends Cubit<ExtendableState> {
       emit(state);
     });
   }
+
+  final Map<Type, ExtendableState> lastStates = {};
+
+  @override
+  // ignore: must_call_super
+  void onChange(Change<ExtendableState> change) {
+    final superStateType = change.nextState.superState;
+    final currentState = change.nextState;
+    final oldSimilarState = lastStates[superStateType];
+
+    if (oldSimilarState == null) {
+      return;
+    }
+
+    final callback = stateObservers[superStateType];
+    if (callback == null) {
+      _defaultStateObserver(oldSimilarState, currentState);
+    } else {
+      callback(oldSimilarState, currentState);
+    }
+
+    lastStates[superStateType] = currentState;
+  }
 }
 
 @immutable
 abstract class ExtendableState {
   const ExtendableState();
+
+  Type get superState;
 }
 
-class _GlobalInitialState extends ExtendableState {}
+class _GlobalInitialState extends ExtendableState {
+  @override
+  Type get superState => _GlobalInitialState;
+}
--- lib/utils/state_observer.dart
+++ lib/utils/state_observer.dart
@@ -1,1 +1,29 @@
-// TODO: make StateObserver which is equvalent to BlocObserver
+part of '../global_blocs.dart';
+
+final Map<Type, StateChanged> stateObservers = {};
+
+StateChanged _defaultStateObserver = (
+  ExtendableState previous,
+  ExtendableState current,
+) {
+  if (kDebugMode) {
+    print(
+      'Scope ${current.superState}: '
+      'Transitioning from ${previous.runtimeType} '
+      'to ${current.runtimeType}',
+    );
+  }
+};
+
+void setDefaulstStateObserver(StateChanged stateChanged) {
+  _defaultStateObserver = stateChanged;
+}
+
+void setStateObserver(Type stateSuperType, StateChanged stateChanged) {
+  stateObservers[stateSuperType] = stateChanged;
+}
+
+typedef StateChanged = void Function(
+  ExtendableState previous,
+  ExtendableState current,
+);
