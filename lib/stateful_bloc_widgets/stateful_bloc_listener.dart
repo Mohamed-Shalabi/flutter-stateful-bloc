db12f98832666ba30842d5bacc4c6868e728899d
Mohamed-Shalabi <mshalaby10@gmail.com>
2023-03-31 10:49:27 +0200

    improved the performance of StatefulBlocListener

--- lib/stateful_bloc_widgets/stateful_bloc_listener.dart
+++ lib/stateful_bloc_widgets/stateful_bloc_listener.dart
@@ -1,20 +1,31 @@
 part of '../global_blocs.dart';
 
-class StatefulBlocListener extends StatelessWidget {
+class StatefulBlocListener<ListenedState extends ExtendableState>
+    extends StatelessWidget {
   const StatefulBlocListener({
     super.key,
     required this.listener,
     required this.body,
   });
 
-  final BlocWidgetListener<ExtendableState> listener;
+  final StateWidgetListener<ListenedState> listener;
   final Widget body;
 
   @override
   Widget build(BuildContext context) {
     return BlocListener<_GlobalCubit, ExtendableState>(
-      listener: listener,
+      listenWhen: (previous, current) {
+        return current is ListenedState && current != previous;
+      },
+      listener: (context, state) {
+        if (state is ListenedState) {
+          listener(context, state);
+        }
+      },
       child: body,
     );
   }
 }
+
+typedef StateWidgetListener<ListenedState extends ExtendableState> = void
+    Function(BuildContext context, ListenedState state);
