59ed73511ad78d20031cbfb804dd8662b658929b
Mohamed-Shalabi <mshalaby10@gmail.com>
2023-04-05 09:29:06 +0200

    incremented the version number

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,3 +17,7 @@
 ## 0.0.5-beta
 
 * Added ObjectProcider<T>.
+
+## 0.0.6-beta
+
+* Solved leakages of ObjectProcider<T>.
--- lib/src/utils_widgets/object_provider.dart
+++ lib/src/utils_widgets/object_provider.dart
@@ -1,3 +1,9 @@
 part of '../../flutter_stateful_bloc.dart';
 
 typedef ObjectProvider<T> = RepositoryProvider<T>;
+
+extension ObjectGrabber on BuildContext {
+  T readObject<T>() => read<T>();
+
+  T watchObject<T>() => watch<T>();
+}
